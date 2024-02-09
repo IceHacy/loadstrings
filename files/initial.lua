2c8af8d09c8439ebdac454d30197a46ebaf818a5
IceHacy <78807447+IceHacy@users.noreply.github.com>
2024-02-09 20:00:15 +0000

    Update initial.lua
    wowie aimbot

--- files/initial.lua
+++ files/initial.lua
@@ -1,6 +1,31 @@
-local player = game.Players.LocalPlayer
-local highlight = Instance.new("SelectionBox")
-highlight.Color3 = Color3.new(1, 1, 1)
-highlight.LineThickness = 0.05
-highlight.Adornee = player.Character.HumanoidRootPart
-highlight.Parent = player.Character
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Camera = game.Workspace.CurrentCamera
+
+local function getNearestPlayer()
+    local maxDist = math.huge
+    local nearestPlayer = nil
+    for _, player in pairs(Players:GetPlayers()) do
+        if player ~= LocalPlayer then
+            local character = player.Character
+            if character and character:FindFirstChild("HumanoidRootPart") then
+                local dist = (character.HumanoidRootPart.Position - Camera.CFrame.Position).magnitude
+                if dist < maxDist then
+                    maxDist = dist
+                    nearestPlayer = character.HumanoidRootPart
+                end
+            end
+        end
+    end
+    return nearestPlayer
+end
+
+local function applyLockOn()
+    local highlight = Instance.new("SelectionBox")
+    highlight.Color3 = Color3.new(1, 1, 1)
+    highlight.LineThickness = 0.05
+    highlight.Adornee = getNearestPlayer()
+    highlight.Parent = game.Workspace
+end
+
+applyLockOn()
